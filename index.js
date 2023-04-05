f1ab15df4284f6e9f79141e12c3e6a782b102cb1
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2023-04-05 19:38:46 -0400

    chat functions

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const fs = require("fs");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
+const jsonParser = bodyParser.json();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function openaiCall() {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello world" }],
-  });
-  return completion.data.choices[0].message.content;
+async function openaiCall(chatMessage) {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: chatMessage }],
+    });
+    return completion.data.choices[0].message.content;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // This method will save the binary content of the request as a file.
@@ -29,10 +35,22 @@ app.post("/binary-upload", async (req, res) => {
 });
 
 // This method will save a "photo" field from the request as a file.
-app.post("/multipart-upload", upload.single("photo"), (req, res) => {
+app.post("/multipart-upload", upload.single("photo"), async (req, res) => {
   // You can access other HTTP parameters. They are located in the body object.
   console.log(req.body);
-  res.end("OK");
+  let answer = await openaiCall("I am uploading a file.");
+  console.log(answer);
+  res.send({ answer: answer });
+});
+
+app.post("/chat", jsonParser, async (req, res) => {
+  console.log(req.body);
+  let answer = await openaiCall(req.body.chat);
+  res.send({ answer: answer });
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
 app.get("/", (req, res) => {
